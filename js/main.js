761a57d1b9246d823a8d6741061c47310bd7eea9
Harika Chengelput <harika317@yahoo.com>
2018-07-24 17:00:10 +0100

    aria updates and grid styling updates

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ createRestaurantHTML = restaurant => {
   image.alt = `restaurant image of ${restaurant.name}`;
 
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
   name.tabIndex = 0;
